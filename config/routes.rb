b7bc699fa8f5295e14b8cb42afca9d107fd27897
Project <projectp907@gmail.com>
2023-05-14 14:55:58 +0530

    backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users
+  post '/login', to: 'authentication#login'
+  post '/signup', to: 'users#create'
+  # get '/*a', to: 'application#not_found'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :products
+
 end
